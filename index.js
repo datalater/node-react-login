1e46cb6e881ab76c9f454afe7e520854d361a423
datalater <the7mincheol@gmail.com>
2020-09-15 19:32:08 +0900

    mongoDB를 연결하라

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+const config = require('./config/key')
+
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello World JMC!')
